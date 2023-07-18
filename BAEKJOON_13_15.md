dbe31f40bc2eaf768162ba6b12a01c8c139d8999
정지헌 <hun01414@naver.com>
2023-07-18 17:33:27 +0900

    2-2최종

--- BAEKJOON_13_15.md
+++ BAEKJOON_13_15.md
@@ -231,3 +231,14 @@ for i in range(M):
     except:
         print(po_dict[int(name)].strip('\n'))
 ```
+- 10816 숫자 카드 2
+```python
+import sys
+input = sys.stdin.readline
+from collections import Counter
+M = int(input())
+ls_M = Counter(map(int, input().split()))
+N = int(input())
+for i in list(map(int, input().split())):
+    print(ls_M[i], end = ' ')
+```
--- test.py
+++ test.py
@@ -1,18 +1,9 @@
 import sys
 input = sys.stdin.readline
-
-N, M = map(int, input().split())
-po_dict = dict()
-for i in range(N):
-    K = input()
-    po_dict[i+1] = K
-
-po_dict2 = {v:k for k,v in po_dict.items()}
-
-for i in range(M):
-    name = input()
-    try:
-        print(po_dict2[name])
-    except:
-        print(po_dict[int(name)].strip('\n'))
+from collections import Counter
+M = int(input())
+ls_M = Counter(map(int, input().split()))
+N = int(input())
+for i in list(map(int, input().split())):
+    print(ls_M[i], end = ' ')
 
