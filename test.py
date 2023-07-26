2028dd3046fada6aaf4d43f6a0b614453ffad716
정지헌 <hun01414@naver.com>
2023-07-26 15:44:45 +0900

    오큰수완

--- test.py
+++ test.py
@@ -1,23 +1,31 @@
 N = int(input())
-ls = []
+ls = list(map(int, input().split()))
 ls_stack = []
+rlt = []
 cnt = 0
-for i in range(N):
-    ls.append(int(input()))
+
 ls_stack.append([ls.pop(-1), -1])
+rlt.append(ls_stack[0][1])
+
+
 for i in range(N-1):
     temp_index = i
+
     temp_item = ls.pop(-1)
-    while ls_stack[temp_index][0] < temp_item:
+
+    while ls_stack[temp_index][0] <= temp_item:
+
         if ls_stack[temp_index][1] == -1:
             ls_stack.append([temp_item, -1])
-            cnt += i +1
+            rlt.append(-1)
             break
+
         else:
             temp_index = ls_stack[temp_index][1]
+
     else:
         ls_stack.append([temp_item, temp_index])
-        cnt += i - temp_index
-print(cnt)
+        rlt.append(ls_stack[temp_index][0])
 
-    
+for i in rlt[::-1]:
+    print(i, end = ' ') 
