0f0ae31b6439dbcaa1206ddce779860877f0670f
정지헌 <hun01414@naver.com>
2023-07-27 11:26:01 +0900

    오아시스개x

--- test.py
+++ test.py
@@ -1,31 +1,25 @@
 N = int(input())
-ls = list(map(int, input().split()))
-ls_stack = []
-rlt = []
+ls = []
+stack_ls = []
+for i in range(N):
+    ls.append(int(input()))
+ls.append(-1)
 cnt = 0
 
-ls_stack.append([ls.pop(-1), -1])
-rlt.append(ls_stack[0][1])
-
-
-for i in range(N-1):
-    temp_index = i
-
-    temp_item = ls.pop(-1)
-
-    while ls_stack[temp_index][0] <= temp_item:
-
-        if ls_stack[temp_index][1] == -1:
-            ls_stack.append([temp_item, -1])
-            rlt.append(-1)
+for i in range(N):
+    temp_long = -1
+    temp_val = ls.pop(0)
+    j = 0
+    while temp_val >= ls[j]:
+        if ls[j] == -1:
             break
-
-        else:
-            temp_index = ls_stack[temp_index][1]
-
+        if temp_val >= temp_long and ls[j] >= temp_long:
+            temp_long = ls[j]
+            j += 1
+            cnt += 1
+        elif ls[j] < temp_long:
+            break
     else:
-        ls_stack.append([temp_item, temp_index])
-        rlt.append(ls_stack[temp_index][0])
+        cnt += 1
 
-for i in rlt[::-1]:
-    print(i, end = ' ') 
+print(cnt)
