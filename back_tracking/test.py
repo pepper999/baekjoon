61aa0ba94d37e2d957de03d592c204bee6873663
정지헌 <hun01414@naver.com>
2023-08-16 11:26:49 +0900

    스타트와 링크

--- back_tracking/test.py
+++ back_tracking/test.py
@@ -1,46 +1,30 @@
+N = int(input())
+visited = [0] * (N+1)
+rlt = 10 ** 5
+matrix = []
+for i in range(N):
+    matrix.append(list(map(int, input().split())))
 
-sdoku = []
-for _ in range(9):
-    sdoku.append(list(map(int, input().split())))
+def back(L, num):
+    global rlt
+    if L == N // 2:
+        start = 0
+        link = 0
+        for i in range(N):
+            for j in range(N):
+                if visited[i] and visited[j] and i != j:
+                    start += matrix[i][j]
+                    start += matrix[j][i]
+                elif not visited[i] and not visited[j] and i != j:
+                    link += matrix[i][j]
+                    link += matrix[j][i]
+                if abs(start - link) < rlt:
+                    rlt = abs(start - link)
+        return rlt
+    for i in range(num, N+1):
+        if not visited[i]:
+            visited[i] = 1
+            back(L+1, num+1)
+            visited[i] = 0
 
-blank = []
-for i in range(9):
-    for j in range(9):
-        if sdoku[i][j] == 0:
-            blank.append([i, j])
-
-def checkrow(x, a):
-    for i in range(9):
-        if a == sdoku[x][i]:
-            return 0
-    return 1
-
-def checkcol(y, a):
-    for i in range(9):
-        if a == sdoku[i][y]:
-            return 0
-    return 1
-
-def checkbox(x, y, a):
-    nx = x // 3 * 3
-    ny = y // 3 * 3
-    for i in range(3):
-        for j in range(3):
-            if a == sdoku[nx+i][ny+j]:
-                return 0
-    return 1
-
-def dfs(i):
-        if i == len(blank):
-            for j in range(9):
-                print(*sdoku[j])
-            return 0
-            
-        x, y = blank[i]
-        for a in range(1, 10):
-            if checkrow(x, a) and checkcol(y, a) and checkbox(x, y, a):
-                sdoku[x][y] = a
-                dfs(i + 1)
-                sdoku[x][y] = 0
-
-dfs(0)
+print(rlt)
