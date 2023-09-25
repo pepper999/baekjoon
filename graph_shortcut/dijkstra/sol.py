935e9644366ff0c15eba104aa226a78d189b1ee0
정지헌 <hun01414@naver.com>
2023-09-25 17:17:45 +0900

    탈옥중

--- graph_shortcut/dijkstra/sol.py
+++ graph_shortcut/dijkstra/sol.py
@@ -1,33 +1,47 @@
 import heapq
+from pprint import pprint
+
+delta = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 def dijkstra(i, j):
-    visited = [[0 for _ in range(w)] for _ in range(h)]
-    heap = [(0,i,j)]
-    visited[i][j] = 1
+    visited = [[float('inf') for _ in range(w+2)] for _ in range(h+2)]
+    heap = [(0, i, j)]
+    visited[i][j] = 0
     while heap:
-        
-
+        weight, x, y = heapq.heappop(heap)
+        if visited[x][y] >= weight:
+            for d in range(4):
+                nx = x + delta[d][0]
+                ny = y + delta[d][1]
+                if 0 <= nx < h and 0 <= ny < w and graph[nx][ny] != '*' and visited[nx][ny] > weight:
+                    if graph[nx][ny] == '#':
+                        visited[nx][ny] = weight + 1
+                        heapq.heappush(heap, (weight + 1, nx, ny))
+                    else:
+                        visited[nx][ny] = weight
+                        heapq.heappush(heap, (weight, nx, ny))
+    return visited
 
 T = int(input())
 for t in range(T):
     h, w = map(int, input().split())
-    graph = []
-    break_spot = []
-    for i in range(h):
-        graph.append(list(input()))
-        if i == 0 or i == h-1:
-            for j in range(w):
-                if graph[i][j] == '#':
-                    break_spot.append([i,j,1])
-                elif graph[i][j] == '.':
-                    break_spot.append([i,j,0])
-        else:
-            if graph[0] == '#':
-                break_spot.append([i,0,1])
-            elif graph[0] == '.':
-                break_spot.append([i,0,0])
-            elif graph[-1] == '#':
-                break_spot.append([i,w-1,1])
-            elif graph[-1] == '.':
-                break_spot.append([i,w-1,0])
-    print(break_spot)
+    graph = [['.' for _ in range(w+2)]]
+    start_point = []
+    for i in range(1, h+1):
+        graph.append(['.'] + list(input()) + ['.'])
+        for j in range(w+2):
+            if graph[i][j] == '$':
+                start_point.append((i, j))
+    graph.append(['.' for _ in range(w+2)])
+    min_steps = float('inf')
+    steps1 = dijkstra(start_point[0][0], start_point[0][1])
+    steps2 = dijkstra(start_point[1][0], start_point[1][1])
+    outer = dijkstra(0, 0)
+    for i in range(h+2):
+        for j in range(w+2):
+            if graph[i][j] != "*":
+                total_steps = steps1[i][j] + steps2[i][j] + outer[i][j]
+                if graph[i][j] == "#":
+                    total_steps -= 2
+                min_steps = min(min_steps, total_steps)
+    print(min_steps)
