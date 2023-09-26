866525eb05a5618512636051eb33cd2e89a9d0a7
정지헌 <hun01414@naver.com>
2023-09-26 15:58:09 +0900

    KCM중

--- graph_shortcut/dijkstra/sol.py
+++ graph_shortcut/dijkstra/sol.py
@@ -1,47 +1,26 @@
 import heapq
-from pprint import pprint
 
-delta = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-def dijkstra(i, j):
-    visited = [[float('inf') for _ in range(w+2)] for _ in range(h+2)]
-    heap = [(0, i, j)]
-    visited[i][j] = 0
+def dijkstra():
+    distances = [[float('inf') for _ in range(max(price)+1)] for _ in range(N+1)]
+    heap = [(0,price[1],1)]
+    distances[1][price[1]] = 0
     while heap:
-        weight, x, y = heapq.heappop(heap)
-        if visited[x][y] >= weight:
-            for d in range(4):
-                nx = x + delta[d][0]
-                ny = y + delta[d][1]
-                if 0 <= nx < h and 0 <= ny < w and graph[nx][ny] != '*' and visited[nx][ny] > weight:
-                    if graph[nx][ny] == '#':
-                        visited[nx][ny] = weight + 1
-                        heapq.heappush(heap, (weight + 1, nx, ny))
-                    else:
-                        visited[nx][ny] = weight
-                        heapq.heappush(heap, (weight, nx, ny))
-    return visited
+        now_weight, now_price, now = heapq.heappop(heap)
+        if now == N:
+            return now_weight
+        if now_weight <= distances[now][now_price]:
+            for next, weight in graph[now]:
+                next_price = min(now_price, price[next])
+                next_weight = now_weight + (weight * now_price)
+                if distances[next][now_price] > next_weight:
+                    distances[next][now_price] = next_weight
+                    heapq.heappush(heap, (next_weight, next_price, next))
 
-T = int(input())
-for t in range(T):
-    h, w = map(int, input().split())
-    graph = [['.' for _ in range(w+2)]]
-    start_point = []
-    for i in range(1, h+1):
-        graph.append(['.'] + list(input()) + ['.'])
-        for j in range(w+2):
-            if graph[i][j] == '$':
-                start_point.append((i, j))
-    graph.append(['.' for _ in range(w+2)])
-    min_steps = float('inf')
-    steps1 = dijkstra(start_point[0][0], start_point[0][1])
-    steps2 = dijkstra(start_point[1][0], start_point[1][1])
-    outer = dijkstra(0, 0)
-    for i in range(h+2):
-        for j in range(w+2):
-            if graph[i][j] != "*":
-                total_steps = steps1[i][j] + steps2[i][j] + outer[i][j]
-                if graph[i][j] == "#":
-                    total_steps -= 2
-                min_steps = min(min_steps, total_steps)
-    print(min_steps)
+N, M = map(int, input().split())
+price = [0] + list(map(int, input().split()))
+graph = [[] for _ in range(N+1)]
+for i in range(M):
+    a, b, w = map(int, input().split())
+    graph[a].append((b,w))
+    graph[b].append((a,w))
+print(dijkstra())
