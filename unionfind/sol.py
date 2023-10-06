a04f14e1f9330f9105b52b83eaf5e882c22a3f39
정지헌 <hun01414@naver.com>
2023-10-06 12:41:07 +0900

    123

--- unionfind/sol.py
+++ unionfind/sol.py
@@ -1,32 +1,27 @@
-import sys
-sys.setrecursionlimit(int(1e6))
-def sol():
-    def find_parents(a):
-        if parents[a] != a:
-            parents[a] = find_parents(parents[a])
-        return parents[a]
+def find(a):
+    if parents[a] != a:
+        parents[a] = find(parents[a])
+    return parents[a]
 
-    def union(a, b):
-        a = find_parents(a)
-        b = find_parents(b)
-        if a < b:
-            parents[b] = a
-        else:
-            parents[a] = b
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a != b:
+        parents[b] = a
+        size[a] += size[b]
+    print(size[a])
 
-    N = int(input())
-    M = int(input())
-    parents = [i for i in range(N+1)]
-    for i in range(1, N+1):
-        edge = list(map(int, input().split()))
-        for j in range(1, N+1):
-            if edge[j-1] == 1:
-                union(i, j)
-    plans = list(map(int, input().split()))
-    for i in range(M-1):
-        if find_parents(plans[i]) != find_parents(plans[i+1]):
-            print('NO')
-            return
-    print('YES')
-
-sol()
+T = int(input())
+for t in range(T):
+    F = int(input())
+    parents = {}
+    size = {}
+    for f in range(F):
+        friend1, friend2 = input().split()
+        if friend1 not in parents:
+            parents[friend1] = friend1
+            size[friend1] = 1
+        if friend2 not in parents:
+            parents[friend2] = friend2
+            size[friend2] = 1
+        union(friend1, friend2)
